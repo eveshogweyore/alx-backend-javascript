5caa68280680c56158a3e52f30134bc55f9d949f
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-08-01 13:05:02 +0100

    Refactor codebase to make use of for...of in place of for...in.

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
+  }
+
+  return array;
+}
